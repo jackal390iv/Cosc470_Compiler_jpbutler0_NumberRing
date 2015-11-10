c6126399df355f30a1cfcb44147a153d35f9ad44
walter <walter@10.103.42.226>
2015-11-09 19:24:31 -0500

    created

---
+++ Number_Ring/src/main/java/cosc489/capstone/number_ring/Node.java
@@ -0,0 +1,13 @@
+/*
+ * Class: COSC 489
+ * Project: Capstone
+ */
+package cosc489.capstone.number_ring;
+
+/**
+ *
+ * @author Jonathan Butler <https://github.com/jackal390iv>
+ */
+public class Node {
+    
+}
